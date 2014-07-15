9694c8e2570daff0e6096f28f40f8d4526010b31
ragingo <save.the.world.crush@gmail.com>
2014-07-15 14:42:19 +0900

    デバッグテーブル追加 ロジック修正

--- bf_test1.sql
+++ bf_test1.sql
@@ -28,16 +28,48 @@ option (MAXRECURSION 0)
 
 go
 
+if object_id('bf_tape_debug') is not null
+	drop table bf_tape_debug
+go
+
+create table bf_tape_debug
+(
+	datetime datetime,
+	count int,
+	pos int,
+	code char(1),
+	value1 int,
+	value2 char(1)
+)
+go
+
 --------------------------------------------------------------------------------------------
-declare @code varchar(max) = '+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.------------.<++++++++.--------.+++.------.--------.>+.'
+-- 1. "Hello World!" 1
+--declare @code varchar(max) = '+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.------------.<++++++++.--------.+++.------.--------.>+.'
+-- 2. "Hello World!" 2
+declare @code varchar(max) = '>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.'
+-- 3. FizzBuzz
+--declare @code varchar(max) = '++++++[->++++>>+>+>-<<<<<]>[<++++>>+++>++++>>+++>+++++>+++++>>>>>>++>>++<<<<<<<<<<<<<<-]<++++>+++>-->+++>->>--->++>>>+++++[->++>++<<]<<<<<<<<<<[->-[>>>>>>>]>[<+++>.>.>>>>..>>>+<]<<<<<-[>>>>]>[<+++++>.>.>..>>>+<]>>>>+<-[<<<]<[[-<<+>>]>>>+>+<<<<<<[->>+>+>-<<<<]<]>>[[-]<]>[>>>[>.<<.<<<]<[.<<<<]>]>.<<<<<<<<<<<]'
+-- 4. “K“–
+--declare @code varchar(max) = ''
+
 declare @codepos int = 1
 declare @ptr int = 1
 declare @output varchar(max) = ''
+declare @counter int = 0
 
 while 1=1
 begin
 	declare @ch char(1) = substring(@code, @codepos, 1)
 	declare @val int = 0
+
+	-- debug
+	begin
+		set @counter += 1
+		--print ('count:' + cast(@counter as varchar) + ', pos:' + cast(@codepos as varchar) + ', ch:' + @ch)
+		--select @val = value from bf_tape where id = @ptr
+		--insert into bf_tape_debug values(SYSDATETIME(), @counter, @codepos, @ch, @val, char(@val))
+	end
 
 	if @ch = ' '
 		break
@@ -51,12 +83,16 @@ begin
 		begin
 			select @val = value from bf_tape where id = @ptr
 			set @val += 1
+			if @val = 256
+				set @val = 0
 			update bf_tape set value = @val where id = @ptr
 		end
 	if @ch = '-'
 		begin
 			select @val = value from bf_tape where id = @ptr
 			set @val -= 1
+			if @val = -1
+				set @val = 255
 			update bf_tape set value = @val where id = @ptr
 		end
 	if @ch = '>'
@@ -70,11 +106,6 @@ begin
 	if @ch = '['
 		begin
 			select @val = value from bf_tape where id = @ptr
-			if @val > 0
-				begin
-					set @codepos += 1
-					continue
-				end
 			if @val = 0
 				begin
 					while 1=1
@@ -90,14 +121,17 @@ begin
 		end
 	if @ch = ']'
 		begin
-			while 1=1
-			begin
-				set @codepos -= 1
-				declare @ch3 char(1) = substring(@code, @codepos, 1)
-				if @ch3 = '['
+			select @val = value from bf_tape where id = @ptr
+			if @val <> 0
+				begin
+					while 1=1
 					begin
 						set @codepos -= 1
-						break
+						declare @ch3 char(1) = substring(@code, @codepos, 1)
+						if @ch3 = '['
+							begin
+								break
+							end
 					end
 			end
 		end
