792f23569c6517f3778ff9a5c347de9879d4c467
ragingo <save.the.world.crush@gmail.com>
2015-06-01 09:53:26 +0900

    Bitmap解析修正

--- SQLServer/image.sql
+++ SQLServer/image.sql
@@ -21,28 +21,24 @@ go
 
 with
 	Param as (
-		select name, convert(varchar(max), data, 2) as data from image_data where name = 'test1'
+		select name, convert(varchar(max), data, 2) as data, data as rawdata from image_data where name = 'test1'
 	),
 	BITMAPFILEHEADER as (
 		select
-			(char(convert(binary(1), substring(p.data, 1, 2), 2)) +
-			 char(convert(binary(1), substring(p.data, 3, 2), 2))
-			) as bfType,
-			cast(substring(p.data, 5, 8) as int) as bfSize, -- これはだめ。LEだからやっぱり関数使いたい
-			cast(substring(p.data, 13, 4) as int) as bfReserved1,
-			cast(substring(p.data, 17, 4) as int) as bfReserved2,
-			cast(substring(p.data, 21, 8) as int) as bfOffBits
+			convert(char(2), convert(binary(2), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 1, 2), 2)) as bfType,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata,  3, 4), 2))) )as bfSize,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata,  7, 2), 2))) )as bfReserved1,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata,  9, 2), 2))) )as bfReserved2,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 11, 4), 2))) )as bfOffBits
 		from
 			Param as p
 	),
 	BITMAPINFOHEADER as (
 		select
-			cast(substring(p.data, 29, 8) as int) as biSize,
-			cast(substring(p.data, 37, 8) as int) as biWidth,
-			cast(substring(p.data, 45, 8) as int) as biHeight,
-			cast(substring(p.data, 53, 4) as int) as biPlanes,
-			substring(p.data, 53, 4) as biPlanes_a,
-			1 as a
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 15, 4), 2))) )as biSize,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 19, 4), 2))) )as biWidth,
+			convert(int, convert(binary(4), reverse(convert(binary(4), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 23, 4), 2))) )as biHeight,
+			convert(int, convert(binary(2), reverse(convert(binary(2), master.dbo.fn_varbintohexsubstring(0, p.rawdata, 27, 2), 2))) )as biPlanes
 		from
 			Param as p
 	),
