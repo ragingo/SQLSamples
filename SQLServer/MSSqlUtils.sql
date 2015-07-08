eadb58ff7a6bdf10439636b822ea4bf5de4b9f8d
ragingo <save.the.world.crush@gmail.com>
2015-07-09 01:08:23 +0900

    SQLServerユーティリティ関数作成

---
+++ SQLServer/MSSqlUtils.sql
@@ -0,0 +1,55 @@
+﻿
+--EXEC sp_configure 'clr enabled' , '1';
+--RECONFIGURE;
+--EXEC sp_configure 'clr enabled';
+
+--go
+
+--drop function [dbo].[Collection_Take]
+--drop function [dbo].[Collection_Skip]
+--drop function [dbo].[Collection_Range]
+--drop function [dbo].[Collection_Length]
+
+go
+
+create function [dbo].[Collection_Take]
+(
+	@binary varbinary(max),
+	@count int
+)
+returns varbinary(max)
+as
+external name [MSSqlUtils].[MSSqlUtils.Collection].[Take]
+
+go
+
+create function [dbo].[Collection_Skip]
+(
+	@binary varbinary(max),
+	@count int
+)
+returns varbinary(max)
+as
+external name [MSSqlUtils].[MSSqlUtils.Collection].[Skip]
+
+go
+
+create function [dbo].[Collection_Range]
+(
+	@binary varbinary(max),
+	@from int,
+	@count int
+)
+returns varbinary(max)
+as
+external name [MSSqlUtils].[MSSqlUtils.Collection].[Range]
+
+go
+
+create function [dbo].[Collection_Length]
+(
+	@binary varbinary(max)
+)
+returns bigint
+as
+external name [MSSqlUtils].[MSSqlUtils.Collection].[Length]
---
+++ SQLServer/MSSqlUtils/MSSqlUtils/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace MSSqlUtils
+{
+	public class Collection
+	{
+		[SqlFunction]
+		public static byte[] Take(SqlBytes binary, int count)
+		{
+			if (binary.IsNull)
+			{
+				return null;
+			}
+			byte[] buf = new byte[count];
+			binary.Read(0, buf, 0, count);
+			return buf;
+		}
+
+		[SqlFunction]
+		public static byte[] Skip(SqlBytes binary, int count)
+		{
+			if (binary.IsNull)
+			{
+				return null;
+			}
+			byte[] buf = new byte[binary.Length - count];
+			binary.Read(count, buf, 0, buf.Length);
+			return buf;
+		}
+
+		[SqlFunction]
+		public static byte[] Range(SqlBytes binary, int from, int count)
+		{
+			if (binary.IsNull)
+			{
+				return null;
+			}
+			byte[] buf = new byte[count];
+			binary.Read(from, buf, 0, count);
+			return buf;
+		}
+
+		[SqlFunction]
+		public static long Length(SqlBytes binary)
+		{
+			return binary.Length;
+		}
+	}
+}
