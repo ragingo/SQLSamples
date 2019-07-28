0b72b7ea5731ba42208b30789f2ab0cfa2245c91
ragingo <save.the.world.crush@gmail.com>
2019-07-28 23:46:56 +0900

    tkhd 追加

--- SQLServer/mp4box_parser.sql
+++ SQLServer/mp4box_parser.sql
@@ -42,17 +42,17 @@ with
             (case convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, next_offset + 4, 4), 1))
                 -- box      : 4 + 4 (size + type)
                 -- full box : 4 + 4 + 4 (box + version 8bit + flag 24bit)
-                when 'moov' then 4 + 4
-                when 'trak' then 4 + 4
-                when 'edts' then 4 + 4
-                when 'mdia' then 4 + 4
-                when 'minf' then 4 + 4
-                when 'dinf' then 4 + 4
-                when 'dref' then 4 + 4 + 4 + 4 -- data reference box : full box + 4 (entry count)
-                when 'stbl' then 4 + 4
-                when 'stsd' then 4 + 4 + 4 + 4 -- sample description box : full box + 4 (entry count)
-                when 'udta' then 4 + 4
-                when 'meta' then 4 + 4 + 4
+                when 'moov' then 8
+                when 'trak' then 8
+                when 'edts' then 8
+                when 'mdia' then 8
+                when 'minf' then 8
+                when 'dinf' then 8
+                when 'dref' then 12 + 4 -- data reference box : full box + 4 (entry count)
+                when 'stbl' then 8
+                when 'stsd' then 12 + 4 -- sample description box : full box + 4 (entry count)
+                when 'udta' then 8
+                when 'meta' then 12
                 else convert(int, convert(varbinary(max), dbo.read_bytes(1, data, next_offset, 4), 1))
             end) + next_offset,
             data
@@ -60,6 +60,22 @@ with
             parse_boxes
         where
             size is not null
+    ),
+    parse_tkhd(size, type, current_offset, next_offset, data, track_id, duration, width, height) as (
+        select
+            size,
+            type,
+            current_offset,
+            next_offset,
+            data,
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 12 + 4 + 4, 4), 1)),
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 12 + 4 + 4 + 4, 8), 1)),
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 12 + 4 + 4 + 4 + 8 + 4 * 2 + 2 + 2 + 2 + 2 + 4 * 9, 2), 1)),
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 12 + 4 + 4 + 4 + 8 + 4 * 2 + 2 + 2 + 2 + 2 + 4 * 9 + 4, 2), 1))
+        from
+            parse_boxes
+        where
+            type = 'tkhd'
     ),
     parse_avc1(size, type, current_offset, next_offset, data, width, height) as (
         select
@@ -69,8 +85,8 @@ with
             next_offset,
             data,
             -- sample entry (6 + 2) + visual sample entry (2 + 2 + 4 * 3 + 2 (w) + 2 (h))
-            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 4 + 4 + 6 + 2 + 2 + 2 + 4 * 3, 2), 1)),
-            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 4 + 4 + 6 + 2 + 2 + 2 + 4 * 3 + 2, 2), 1))
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 8 + 6 + 2 + 2 + 2 + 4 * 3, 2), 1)),
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 8 + 6 + 2 + 2 + 2 + 4 * 3 + 2, 2), 1))
         from
             parse_boxes
         where
