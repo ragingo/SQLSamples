2befc517d4c6f9732556981775332e3841b1db12
ragingo <save.the.world.crush@gmail.com>
2019-07-28 20:00:25 +0900

    avc1からサイズを取得

--- SQLServer/mp4box_parser.sql
+++ SQLServer/mp4box_parser.sql
@@ -25,19 +25,21 @@ go
 
 
 with
-    parse_boxes(size, type, offset, data) as (
+    parse_boxes(size, type, current_offset, next_offset, data) as (
         select
             convert(int, convert(varbinary(max), dbo.read_bytes(1, data, 1, 4), 1)),
             convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, 1 + 4, 4), 1)),
+            1,
             convert(int, convert(varbinary(max), dbo.read_bytes(1, data, 1, 4), 1)) + 1,
             data
         from
             video
         union all
         select
-            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, offset, 4), 1)),
-            convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, offset + 4, 4), 1)),
-            (case convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, offset + 4, 4), 1))
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, next_offset, 4), 1)),
+            convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, next_offset + 4, 4), 1)),
+            next_offset,
+            (case convert(varchar(max), convert(varbinary(max), dbo.read_bytes(1, data, next_offset + 4, 4), 1))
                 -- box      : 4 + 4 (size + type)
                 -- full box : 4 + 4 + 4 (box + version 8bit + flag 24bit)
                 when 'moov' then 4 + 4
@@ -49,21 +51,42 @@ with
                 when 'dref' then 4 + 4 + 4 + 4 -- data reference box : full box + 4 (entry count)
                 when 'stbl' then 4 + 4
                 when 'stsd' then 4 + 4 + 4 + 4 -- sample description box : full box + 4 (entry count)
-                --when 'avc1' then 4 + 4
                 when 'udta' then 4 + 4
                 when 'meta' then 4 + 4 + 4
-                else convert(int, convert(varbinary(max), dbo.read_bytes(1, data, offset, 4), 1))
-            end) + offset,
+                else convert(int, convert(varbinary(max), dbo.read_bytes(1, data, next_offset, 4), 1))
+            end) + next_offset,
             data
         from
             parse_boxes
         where
             size is not null
+    ),
+    parse_avc1(size, type, current_offset, next_offset, data, width, height) as (
+        select
+            size,
+            type,
+            current_offset,
+            next_offset,
+            data,
+            -- sample entry (6 + 2) + visual sample entry (2 + 2 + 4 * 3 + 2 (w) + 2 (h))
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 4 + 4 + 6 + 2 + 2 + 2 + 4 * 3, 2), 1)),
+            convert(int, convert(varbinary(max), dbo.read_bytes(1, data, current_offset + 4 + 4 + 6 + 2 + 2 + 2 + 4 * 3 + 2, 2), 1))
+        from
+            parse_boxes
+        where
+            type = 'avc1'
+    ),
+    dump as (
+        select
+            size,
+            type,
+            current_offset - 1 as current_offset,
+            next_offset - 1 as next_offset
+        from
+            parse_boxes
     )
 select
-    size,
-    type,
-    offset - 1 as next_offset
+    *
 from
-    parse_boxes
+    dump
 option (maxrecursion 100)
